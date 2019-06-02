ed5dbf859fdd63779d07377e12f25673b1cb7038
Seunghak-Kwak <kdhak2@gmail.com>
2019-06-02 18:01:03 +0900

    change design

--- mysite/views.py
+++ mysite/views.py
@@ -37,42 +37,11 @@ def submit_ok(request):
         manage_price = '월세에 포함',
         detail = "".join(items['detail'])).save()
 
-        #Data.publish(Data)
-
-
         print(items)
 
-
-
-        
         context = {
             'nickname': nickname
         }
 
 
- 
-        
-
-
-
     return render(request, 'mysite/submit_ok.html', context=context)
-
-
-
-
-# def contact(request):
-#     if request.method == 'POST': # If the form has been submitted...
-#         form = ContactForm(request.POST) # A form bound to the POST data
-#         if form.is_valid(): # All validation rules pass
-#             # Process the data in form.cleaned_data
-#             # ...
-
-#             print form.cleaned_data['my_form_field_name']
-
-#             return HttpResponseRedirect('/thanks/') # Redirect after POST
-#     else:
-#         form = ContactForm() # An unbound form
-
-#     return render_to_response('contact.html', {
-#         'form': form,
-#     })
